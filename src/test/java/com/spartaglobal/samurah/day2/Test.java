408e028db58b63c0676a0d6e73858963ff3d23af
Andrei Pavel <samurahh@gmail.com>
2020-12-21 21:32:54 +0000

    First commit

---
+++ src/test/java/com/spartaglobal/samurah/day2/Test.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.samurah.day2;
+
+
+import org.junit.jupiter.api.Assertions;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    void findTwoThatSumsX(){
+
+    }
+
+}
